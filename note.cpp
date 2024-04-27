f50826e8dfc967944fc3ad2968dd30b736635f33
joooons <joon.s.chang@gmail.com>
2024-04-26 23:49:03 -0700

    try string function; ignore .vscode

--- note.cpp
+++ note.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
+
+// using namespace std;
+
+void isSubsequence(std::string s, std::string t)
+{
+    std::cout << s;
+}
 
 int main()
 {
-    std::cout << "testing";
+    std::cout << "--- running script ---"
+              << "\n";
+    std::string s = "test";
+
+    isSubsequence(s, std::string("bogus"));
     return 0;
 }
