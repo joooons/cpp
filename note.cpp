68a2d025c6c5620a3ea257299bbe6445601ffa84
joooons <joon.s.chang@gmail.com>
2024-04-27 00:17:47 -0700

    isSubsequence()

--- note.cpp
+++ note.cpp
@@ -1,19 +1,52 @@
 #include <iostream>
 #include <string>
 
-// using namespace std;
+using namespace std;
 
-void isSubsequence(std::string s, std::string t)
+bool isSubsequence(string s, string t)
 {
-    std::cout << s;
+    cout << s << " " << t << "\n";
+
+    if (s.length() == 0)
+    {
+        cout << "True";
+        return true;
+    }
+
+    if (s.length() > t.length())
+    {
+        cout << "False";
+        return false;
+    }
+
+    int pointer = 0;
+
+    for (int i = 0; i < t.length(); i++)
+    {
+        cout << t[i];
+        if (s[pointer] == t[i])
+        {
+            cout << " found";
+            pointer++;
+            if (pointer == s.length())
+            {
+                cout << "\n"
+                     << "True";
+                return true;
+            }
+        }
+        cout << "\n";
+    }
+
+    cout << "False in the end";
+    return false;
 }
 
 int main()
 {
-    std::cout << "--- running script ---"
-              << "\n";
-    std::string s = "test";
-
-    isSubsequence(s, std::string("bogus"));
+    cout << "--- running script ---"
+         << "\n";
+    // string s = "test";
+    isSubsequence(string(""), string("abkemces"));
     return 0;
 }
