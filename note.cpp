c2f0a722c39b7e97be7e6f1c5aef402b72293dc6
joooons <joon.s.chang@gmail.com>
2024-04-27 17:24:11 -0700

    minimumTotal()

--- note.cpp
+++ note.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -42,11 +44,32 @@ bool isSubsequence(string s, string t)
     return false;
 }
 
+int minimumTotal(vector<vector<int>> &triangle)
+{
+    for (int i = triangle.size() - 2; i > -1; --i)
+    {
+        cout << "line " << i << " - ";
+        for (int j = 0; j < triangle.at(i).size(); j++)
+        {
+            int minVal = min(triangle.at(i + 1).at(j), triangle.at(i + 1).at(j + 1));
+            cout << minVal << " ";
+            triangle.at(i).at(j) = triangle.at(i).at(j) + minVal;
+        }
+        cout << "\n";
+    }
+    return triangle.at(0).at(0);
+}
+
 int main()
 {
     cout << "--- running script ---"
          << "\n";
-    // string s = "test";
-    isSubsequence(string(""), string("abkemces"));
+
+    // isSubsequence(string(""), string("abkemces"));
+
+    vector<vector<int>> arr = {{1}, {3, 2}, {5, 8, 4}};
+    // vector<vector<int>> arr = {{-10}};
+    cout << minimumTotal(arr);
+
     return 0;
 }
