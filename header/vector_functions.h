6472ff28d3914470a9e385d7d80f07d8727c3cb2
joooons <joon.s.chang@gmail.com>
2024-05-06 13:55:39 -0700

    vectorOfInt() to generate vector of random int

--- header/vector_functions.h
+++ header/vector_functions.h
@@ -7,3 +7,5 @@ using namespace std;
 void coutVector(vector<int>);
 
 string showVector(vector<int>);
+
+vector<int> vectorOfInt(int min, int max, int len);
--- sources/vector_functions.cpp
+++ sources/vector_functions.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
@@ -21,4 +23,15 @@ string showVector(vector<int> arr)
         r = r + to_string(arr[i]) + " ";
     }
     return r;
+}
+
+vector<int> vectorOfInt(int min, int max, int len)
+{
+    vector<int> arr;
+    srand(time(NULL));
+    for (int i = 0; i < len; i++)
+    {
+        arr.push_back(rand() % (max - min + 1) + min);
+    }
+    return arr;
 }
