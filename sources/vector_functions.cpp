3efe92dd92f3d49bfd1271bba9eb797a39471120
joooons <joon.s.chang@gmail.com>
2024-05-08 15:22:09 -0700

    add comments to vector_functions

--- sources/vector_functions.cpp
+++ sources/vector_functions.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void coutVector(vector<int> arr)
 {
+    // outputs each integer element of vector arr in the command line.
     for (auto i = arr.begin(); i != arr.end(); ++i)
     {
         cout << *i << " ";
@@ -17,19 +18,22 @@ void coutVector(vector<int> arr)
 
 string showVector(vector<int> arr)
 {
-    string r = "";
-    for (int i = 0; i < arr.size(); ++i)
+    // returns a string of each integer of vector arr separated by a space.
+    string r = to_string(arr[0]);
+    for (int i = 1; i < arr.size(); ++i)
     {
-        r = r + to_string(arr[i]) + " ";
+        r = r + " " + to_string(arr[i]);
     }
     return r;
 }
 
-vector<int> vectorOfInt(int min, int max, int len)
+vector<int> vectorOfInt(int min, int max, int k)
 {
+    // generates a vector of k length, consisting of integers randomly selected
+    // from a range of min to max, inclusive.
     vector<int> arr;
     srand(time(NULL));
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < k; i++)
     {
         arr.push_back(rand() % (max - min + 1) + min);
     }
